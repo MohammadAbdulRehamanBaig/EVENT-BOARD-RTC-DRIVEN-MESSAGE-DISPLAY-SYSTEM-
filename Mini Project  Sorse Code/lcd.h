357bc92c17d1c11f6c99bf4e3a8f0816cec1454b
MohammadAbdulRehamanBaig <163999580+MohammadAbdulRehamanBaig@users.noreply.github.com>
2026-06-25 15:50:13 +0530

    Change StrLCD parameter type from char* to u8*
    Updated function parameter types for StrLCD.

--- Mini Project  Sorse Code/lcd.h
+++ Mini Project  Sorse Code/lcd.h
@@ -1,19 +1,16 @@
 #include "types.h"
-void WriteLCD(u8 data);             // Write data byte to LCD
-void CmdLCD(u8 cmd);                // Send command to LCD
-void CharLCD(u8 ascii);             // Display one ASCII character
-void InitLCD(void);                 // Initialize LCD module
-void StrLCD(char* str);             // Display string on LCD
-void U32LCD(u32 n);                 // Display unsigned 32-bit number
-void S32LCD(s32 n);                 // Display signed 32-bit number
-void BinLCD(u32 n, u8 nbd);         // Display number in binary
-void BuildCGRAM(u8* p, u8 nBytes);  // Create custom LCD characters
-void f32LCD(f32 fnum, u8 ndp);      // Display floating-point number
-void OctLCD(u32 n);                 // Display number in octal
-void HexLCD(u32 n);                 // Display number in hexadecimal
-void ScrollLCD(const char *msg);    // Scroll text message on LCD
-
-
+void WriteLCD(u8 data);
+void CmdLCD(u8 cmd);
+void CharLCD(u8 ascii);
+void InitLCD(void);
+void StrLCD(u8* str);
+void U32LCD(u32 n);
+void S32LCD(s32 n);
+void BinLCD(u32 n, u8 nbd);
+void BuildCGRAM(u8* p, u8 nBytes);
+void f32LCD(f32 fnum, u8 ndp);
+void OctLCD(u32 n);
+void HexLCD(u32 n);
 
 
 
